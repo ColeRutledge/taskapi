8d8e0503e91f5b9b00e07c72793df5d054224aad
Cole Rutledge <ColeRutledge@gmail.com>
2020-11-19 12:06:07 -0500

    adds project, columns, tasks schemas

--- app/schemas.py
+++ app/schemas.py
@@ -1,26 +1,76 @@
 from typing import Optional
 from pydantic import BaseModel
+from sqlalchemy.sql.sqltypes import DateTime
 
 
-class TeamBase(BaseModel):
+class Team(BaseModel):
+    id: int
     team_name: str
 
+    class Config:
+        orm_mode = True
 
-class Team(TeamBase):
+
+class User(BaseModel):
     id: int
-
-
-class UserBase(BaseModel):
     first_name: str
     last_name: str
     email: str
-
-
-class UserCreate(BaseModel):
     password: str
-
-
-class User(UserBase):
-    id: int
     team_id: Optional[int] = None
-    team: Team
+
+    class Config:
+        orm_mode = True
+
+
+class Project(BaseModel):
+    id: int
+    project_name: str
+    team_id: int
+
+    class Config:
+        orm_mode = True
+
+
+class Column(BaseModel):
+    id: int
+    column_name: str
+    column_pos: int
+    project_id: int
+
+    class Config:
+        orm_mode = True
+
+
+class Task(BaseModel):
+    id: int
+    task_description: str
+    due_date: Optional[DateTime] = None
+    column_id: int
+    column_idx: int
+
+    class Config:
+        orm_mode = True
+
+# class TeamBase(BaseModel):
+#     team_name: str
+
+
+# class Team(TeamBase):
+#     id: int
+
+
+# class UserBase(BaseModel):
+#     first_name: str
+#     last_name: str
+#     email: str
+
+
+# class UserCreate(BaseModel):
+#     password: str
+
+
+# class User(UserBase):
+#     id: int
+#     team_id: Optional[int] = None
+#     team: Team
