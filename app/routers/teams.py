c7f6f5372c537e6b3591f89aacfb0dbcd4ea1697
Cole Rutledge <ColeRutledge@gmail.com>
2020-11-20 10:24:56 -0500

    adds get_team by id

--- app/routers/teams.py
+++ app/routers/teams.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from app.db import get_db
 from app import crud, schemas
 from sqlalchemy.orm.session import Session
@@ -10,6 +10,17 @@ router = APIRouter()
 @router.get('/', response_model=list[schemas.Team])
 def get_teams(db: Session = Depends(get_db)):
     return crud.get_teams(db=db)
+
+
+@router.get('/{team_id}', response_model=schemas.Team)
+def get_team(team_id: int, db: Session = Depends(get_db)):
+    db_team = crud.get_team(db, team_id=team_id)
+    if db_team is None:
+        raise HTTPException(
+            status_code=404,
+            detail='Team not found',
+        )
+    return db_team
 
 
 @router.post('/', response_model=schemas.Team)
