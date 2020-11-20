262b48bc2ceea7828a36982ebc776b05dffb2991
Cole Rutledge <ColeRutledge@gmail.com>
2020-11-20 11:56:42 -0500

    adds delete_project

--- app/routers/projects.py
+++ app/routers/projects.py
@@ -26,3 +26,15 @@ def get_project(project_id: int, db: Session = Depends(get_db)):
 @router.post('/', response_model=schemas.Project)
 def create_project(project: schemas.ProjectBase, db: Session = Depends(get_db)):
     return crud.create_project(db=db, project=project)
+
+
+@router.delete('/{project_id}', response_model=schemas.Project)
+def delete_project(project_id: int, db: Session = Depends(get_db)):
+    db_project = crud.get_project(db, project_id=project_id)
+    if db_project is None:
+        raise HTTPException(
+            status_code=404,
+            detail='Project not found',
+        )
+    crud.delete_project(db, project=db_project)
+    return db_project
