527eabadd0cfb50cff97a32e5f21bc077876e4fa
Cole Rutledge <ColeRutledge@gmail.com>
2020-11-20 10:22:48 -0500

    adds get_user by id

--- app/routers/users.py
+++ app/routers/users.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from app.db import get_db
 from app import crud, schemas
 from sqlalchemy.orm.session import Session
@@ -10,6 +10,17 @@ router = APIRouter()
 @router.get('/', response_model=list[schemas.User])
 def get_users(db: Session = Depends(get_db)):
     return crud.get_users(db=db)
+
+
+@router.get('/{user_id}', response_model=schemas.User)
+def get_user(user_id: int, db: Session = Depends(get_db)):
+    db_user = crud.get_user(db, user_id=user_id)
+    if db_user is None:
+        raise HTTPException(
+            status_code=404,
+            detail='User not found',
+        )
+    return db_user
 
 
 @router.post('/', response_model=schemas.User)
