ee0de6d86fede61c839fa92a11d73531d431a293
Cole Rutledge <ColeRutledge@gmail.com>
2020-11-20 10:26:25 -0500

    adds get_project by id

--- app/routers/projects.py
+++ app/routers/projects.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from app.db import get_db
 from app import crud, schemas
 from sqlalchemy.orm.session import Session
@@ -10,6 +10,17 @@ router = APIRouter()
 @router.get('/', response_model=list[schemas.Project])
 def get_teams(db: Session = Depends(get_db)):
     return crud.get_projects(db=db)
+
+
+@router.get('/{project_id}', response_model=schemas.Project)
+def get_project(project_id: int, db: Session = Depends(get_db)):
+    db_project = crud.get_project(db, project_id=project_id)
+    if db_project is None:
+        raise HTTPException(
+            status_code=404,
+            detail='Project not found',
+        )
+    return db_project
 
 
 @router.post('/', response_model=schemas.Project)
