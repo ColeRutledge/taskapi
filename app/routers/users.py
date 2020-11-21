d7761d8c69d379e4e1b44fc562b9abee6876bc68
Cole Rutledge <ColeRutledge@gmail.com>
2020-11-21 18:55:54 -0500

    adds get_user_team

--- app/routers/users.py
+++ app/routers/users.py
@@ -23,6 +23,17 @@ def get_user(user_id: int, db: Session = Depends(get_db)):
     return db_user
 
 
+@router.get('/{user_id}/team', response_model=schemas.Team)
+def get_user_team(user_id: int, db: Session = Depends(get_db)):
+    db_user = crud.get_user(db, user_id=user_id)
+    if db_user is None:
+        raise HTTPException(
+            status_code=404,
+            detail='Project not found',
+        )
+    return db_user.team
+
+
 @router.put('/{user_id}', response_model=schemas.User)
 def update_user(
     user_id: int,
