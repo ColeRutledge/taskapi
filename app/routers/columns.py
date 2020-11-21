b10b78f2dede56a2b02d56a391d37caeb2e84fa5
Cole Rutledge <ColeRutledge@gmail.com>
2020-11-20 19:03:57 -0500

    adds update_column

--- app/routers/columns.py
+++ app/routers/columns.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException, Body
 from app.db import get_db
 from app import crud, schemas
 from sqlalchemy.orm.session import Session
@@ -26,6 +26,21 @@ def get_column(column_id: int, db: Session = Depends(get_db)):
 @router.post('/', response_model=schemas.Column)
 def create_column(column: schemas.ColumnBase, db: Session = Depends(get_db)):
     return crud.create_column(db=db, column=column)
+
+
+@router.put('/{column_id}', response_model=schemas.Column)
+def update_column(
+    column_id: int,
+    column: schemas.ColumnBase = Body(..., embed=True),
+    db: Session = Depends(get_db),
+):
+    db_column = crud.get_column(db, column_id=column_id)
+    if db_column is None:
+        raise HTTPException(
+            status_code=404,
+            detail='Column not found',
+        )
+    return crud.update_column(db, schema=column, model=db_column)
 
 
 @router.delete('/{column_id}', response_model=schemas.Column)
