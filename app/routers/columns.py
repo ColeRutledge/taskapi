90d4dae1d7f133215e6a59dd186928efa4abf39c
Cole Rutledge <ColeRutledge@gmail.com>
2020-11-21 18:41:20 -0500

    adds get_column_tasks

--- app/routers/columns.py
+++ app/routers/columns.py
@@ -23,6 +23,17 @@ def get_column(column_id: int, db: Session = Depends(get_db)):
     return db_column
 
 
+@router.get('/{column_id}/tasks', response_model=list[schemas.Task])
+def get_column_tasks(column_id: int, db: Session = Depends(get_db)):
+    db_column = crud.get_column(db, column_id=column_id)
+    if db_column is None:
+        raise HTTPException(
+            status_code=404,
+            detail='Column not found',
+        )
+    return db_column.tasks
+
+
 @router.post('/', response_model=schemas.Column)
 def create_column(column: schemas.ColumnBase, db: Session = Depends(get_db)):
     return crud.create_column(db=db, column=column)
