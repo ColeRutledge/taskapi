6eb6f534614cc84ce6731cbda41608cf1fb139c0
Cole Rutledge <ColeRutledge@gmail.com>
2020-11-21 21:53:50 -0500

    adds error handling for dupe user reg

--- app/routers/users.py
+++ app/routers/users.py
@@ -62,4 +62,7 @@ def delete_user(user_id: int, db: Session = Depends(get_db)):
 
 @router.post('/', response_model=schemas.User)
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    db_user = crud.get_user_by_email(db, email=user.email)
+    if db_user:
+        raise HTTPException(status_code=400, detail="Email already registered")
     return crud.create_user(db=db, user=user)
