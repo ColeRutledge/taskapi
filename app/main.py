29cd2f940c3be9e56c6fbf614661a11931786825
Cole Rutledge <ColeRutledge@gmail.com>
2020-11-23 20:30:13 -0500

    adds auth to all required routers

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from app import config, models
 from app.db import engine
 from app.routers import users, teams, projects, columns, tasks
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from functools import lru_cache
 
 
@@ -16,9 +16,32 @@ models.Base.metadata.create_all(bind=engine)
 
 
 from app.auth import auth_router
+from app.auth.auth_utils import get_current_user
+
+
 app.include_router(auth_router.router, tags=['Auth'])
-app.include_router(users.router, tags=['Users'], prefix='/users')
-app.include_router(teams.router, tags=['Teams'], prefix='/teams')
-app.include_router(projects.router, tags=['Projects'], prefix='/projects')
-app.include_router(columns.router, tags=['Columns'], prefix='/columns')
-app.include_router(tasks.router, tags=['Tasks'], prefix='/tasks')
+app.include_router(
+    users.router,
+    tags=['Users'], prefix='/users',
+    dependencies=[Depends(get_current_user)]
+)
+app.include_router(
+    teams.router,
+    tags=['Teams'], prefix='/teams',
+    dependencies=[Depends(get_current_user)]
+)
+app.include_router(
+    projects.router,
+    tags=['Projects'], prefix='/projects',
+    dependencies=[Depends(get_current_user)]
+)
+app.include_router(
+    columns.router,
+    tags=['Columns'], prefix='/columns',
+    dependencies=[Depends(get_current_user)]
+)
+app.include_router(
+    tasks.router,
+    tags=['Tasks'], prefix='/tasks',
+    dependencies=[Depends(get_current_user)],
+)
