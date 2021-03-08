b3cb922d94007984ab6efbadd0d3335699a7bf89
Cole Rutledge <ColeRutledge@gmail.com>
2021-03-08 12:43:12 -0500

    update Dockerfile to prep for CI/CD

--- docker/start-reload.sh
+++ docker/start-reload.sh
@@ -3,11 +3,13 @@ set -e
 
 if [ -f /app/app/main.py ]; then
     DEFAULT_MODULE_NAME=app.main
-elif [ -f /app/main.py ]; then
+elif [ -f ./app/main.py ]; then                         # added
     DEFAULT_MODULE_NAME=main
 fi
+
 MODULE_NAME=${MODULE_NAME:-$DEFAULT_MODULE_NAME}
 VARIABLE_NAME=${VARIABLE_NAME:-app}
+
 export APP_MODULE=${APP_MODULE:-"$MODULE_NAME:$VARIABLE_NAME"}
 
 HOST=${HOST:-0.0.0.0}
@@ -15,7 +17,7 @@ PORT=${PORT:-80}
 LOG_LEVEL=${LOG_LEVEL:-info}
 
 # If there's a prestart.sh script in the /app directory or other path specified, run it before starting
-PRE_START_PATH=${PRE_START_PATH:-/app/prestart.sh}
+PRE_START_PATH=${PRE_START_PATH:-./app/prestart.sh}     # added
 echo "Checking for script in $PRE_START_PATH"
 if [ -f $PRE_START_PATH ] ; then
     echo "Running script $PRE_START_PATH"
--- docker/start.sh
+++ docker/start.sh
@@ -6,22 +6,37 @@ if [ -f /app/app/main.py ]; then
 elif [ -f /app/main.py ]; then
     DEFAULT_MODULE_NAME=main
 fi
+
 MODULE_NAME=${MODULE_NAME:-$DEFAULT_MODULE_NAME}
 VARIABLE_NAME=${VARIABLE_NAME:-app}
+
 export APP_MODULE=${APP_MODULE:-"$MODULE_NAME:$VARIABLE_NAME"}
 
 if [ -f /app/gunicorn_conf.py ]; then
     DEFAULT_GUNICORN_CONF=/app/gunicorn_conf.py
 elif [ -f /app/app/gunicorn_conf.py ]; then
     DEFAULT_GUNICORN_CONF=/app/app/gunicorn_conf.py
+elif [ -f /docker/gunicorn_conf.py ]; then
+    DEFAULT_GUNICORN_CONF=/docker/gunicorn_conf.py
 else
     DEFAULT_GUNICORN_CONF=/gunicorn_conf.py
 fi
 export GUNICORN_CONF=${GUNICORN_CONF:-$DEFAULT_GUNICORN_CONF}
 export WORKER_CLASS=${WORKER_CLASS:-"uvicorn.workers.UvicornWorker"}
 
+# ls ../
+# echo $DEFAULT_MODULE_NAME
+# echo $MODULE_NAME
+# echo $VARIABLE_NAME
+# echo $APP_MODULE
+# echo $DEFAULT_GUNICORN_CONF
+# echo $GUNICORN_CONF
+# echo $WORKER_CLASS
+
 # If there's a prestart.sh script in the /app directory or other path specified, run it before starting
 PRE_START_PATH=${PRE_START_PATH:-/app/prestart.sh}
+
+echo $PRE_START_PATH
 echo "Checking for script in $PRE_START_PATH"
 if [ -f $PRE_START_PATH ] ; then
     echo "Running script $PRE_START_PATH"
