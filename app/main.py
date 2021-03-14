b0c4e56b3863269225713a6be7036bc22d07715c
Cole Rutledge <ColeRutledge@gmail.com>
2021-03-13 22:11:21 -0500

    move tests dir to root and create app factory pattern

--- app/main.py
+++ app/main.py
@@ -1,58 +1,47 @@
 from fastapi import FastAPI, Depends, templating, responses, Request, staticfiles
 
-from app import config
 from app.tag_meta import tags_metadata
 from app.auth import auth_router
 from app.auth.auth_utils import get_current_user
 from app.routers import users, teams, projects, columns, tasks
 
 
-app = FastAPI(
-    title='Asana FastAPI',
-    description='FastAPI Python server for an Asana clone',
-    openapi_tags=tags_metadata,
-)
-app_config = config.get_settings()
+def create_application() -> FastAPI:
+    application = FastAPI(
+        title='Asana FastAPI',
+        openapi_tags=tags_metadata,
+        description='FastAPI Python server for an Asana clone')
+    application.mount(
+        path='/static', name='static',
+        app=staticfiles.StaticFiles(directory='app/static'))
+
+    application.include_router(auth_router.router, tags=['Auth'])
+    application.include_router(users.router, tags=['Users'], prefix='/users')
+
+    protected_routers = [
+        (teams.router, 'Teams', '/teams'),
+        (projects.router, 'Projects', '/projects'),
+        (columns.router, 'Columns', '/columns'),
+        (tasks.router, 'Tasks', '/tasks')]
+    for router, tag, prefix in protected_routers:
+        application.include_router(
+            router=router, tags=[tag], prefix=prefix,
+            dependencies=[Depends(get_current_user)])
+
+    return application
+
+
+app = create_application()
+
 templates = templating.Jinja2Templates(directory='app/templates')
-
-
-app.mount('/static', staticfiles.StaticFiles(directory='app/static'), name='static')
 
 
 @app.get('/', response_class=responses.HTMLResponse)
 async def user_login(request: Request):
     return templates.TemplateResponse(
-        'index.html', {'request': request, 'request_headers': request._headers._list}
-    )
+        'index.html', {'request': request, 'request_headers': request._headers._list})
 
 
 @app.get('/items/{id}', response_class=responses.HTMLResponse)
 async def read_item(request: Request, id: str):
     return templates.TemplateResponse("item.html", {"request": request, "id": id})
-
-
-app.include_router(auth_router.router, tags=['Auth'])
-app.include_router(
-    users.router,
-    tags=['Users'], prefix='/users',
-)
-app.include_router(
-    teams.router,
-    tags=['Teams'], prefix='/teams',
-    dependencies=[Depends(get_current_user)],
-)
-app.include_router(
-    projects.router,
-    tags=['Projects'], prefix='/projects',
-    dependencies=[Depends(get_current_user)],
-)
-app.include_router(
-    columns.router,
-    tags=['Columns'], prefix='/columns',
-    dependencies=[Depends(get_current_user)],
-)
-app.include_router(
-    tasks.router,
-    tags=['Tasks'], prefix='/tasks',
-    dependencies=[Depends(get_current_user)],
-)
