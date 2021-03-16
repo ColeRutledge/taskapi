80aeb5625406dac42e39d7ef8770403696f8bbf6
Cole Rutledge <ColeRutledge@gmail.com>
2021-03-16 14:35:33 -0400

    remove 2nd cache location and code cleanup

--- docker/release.py
+++ docker/release.py
@@ -1,12 +1,9 @@
 #! /usr/bin/env python3
 
-from urllib import request, error
-
 import json
 import os
 import subprocess
-
-# import requests
+from urllib import request, error
 
 
 HEROKU_AUTH_TOKEN = os.environ.get('HEROKU_AUTH_TOKEN')
@@ -16,29 +13,22 @@ WEB_DOCKER_IMAGE_ID = subprocess.run([
     'registry.heroku.com/asana-fastapi/web:latest',
     '--format={{.Id}}'], capture_output=True, encoding='utf-8').stdout.strip()
 
-data = {"updates": [{"type": "web", "docker_image": f"{WEB_DOCKER_IMAGE_ID}"}]}
-# data = parse.urlencode(data)
-# data = data.encode('ascii')
+data = {
+    "updates": [{"type": "web", "docker_image": f"{WEB_DOCKER_IMAGE_ID}"}]}
 headers = {
     "Content-Type": "application/json",
     "Accept": "application/vnd.heroku+json; version=3.docker-releases",
     "Authorization": f"Bearer {HEROKU_AUTH_TOKEN}"}
 
-
-# r = requests.patch(
-#     data=json.dumps(data),
-#     headers=headers,
-#     url="https://api.heroku.com/apps/asana-fastapi/formation")
-
-res = request.Request(
+r = request.Request(
     url="https://api.heroku.com/apps/asana-fastapi/formation",
     data=json.dumps(data).encode('utf-8'),
     method='PATCH',
     headers=headers)
 
 try:
-    with request.urlopen(res) as response:
-        html = response.read()  # .decode('utf-8')
+    with request.urlopen(r) as response:
+        html = response.read().decode('utf-8')
         print(html)
 except error.HTTPError as e:
-    print(e.reason)
+    raise SystemExit(f'Error with request: {e.reason}')
