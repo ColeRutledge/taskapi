376ec7cfefefb5428b0eb1e353625bb37634bb56
Cole Rutledge <ColeRutledge@gmail.com>
2021-03-16 10:29:11 -0400

    round 4

--- docker/release.py
+++ docker/release.py
@@ -12,19 +12,21 @@ WEB_DOCKER_IMAGE_ID = subprocess.run([
     'registry.heroku.com/asana-fastapi/web:latest',
     '--format={{.Id}}'], capture_output=True, encoding='utf-8').stdout
 
-# data = {"updates": [{"type": "web", "docker_image": f"{WEB_DOCKER_IMAGE_ID}"}]}
-# data = parse.urlencode(data)
-# data = data.encode('ascii')
+data = {"updates": [{"type": "web", "docker_image": f"{WEB_DOCKER_IMAGE_ID}"}]}
+data = parse.urlencode(data)
+data = data.encode('ascii')
 headers = {
     "Content-Type": "application/json",
     "Accept": "application/vnd.heroku+json; version=3.docker-releases",
     "Authorization": f"Bearer {HEROKU_AUTH_TOKEN}"}
 
-res = request.Request(
-    url="https://api.heroku.com/apps/asana-fastapi/formation",
-    # data=data,
-    method='PATCH',
-    headers=headers)
+res = request.Request(url="https://api.heroku.com/apps/asana-fastapi/formation")
+
+# res = request.Request(
+#     url="https://api.heroku.com/apps/asana-fastapi/formation",
+#     data=data,
+#     method='PATCH',
+#     headers=headers)
 
 try:
     with request.urlopen(res) as response:
