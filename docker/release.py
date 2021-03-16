de64a421b5582d27908ed2460dc5d903623cb166
Cole Rutledge <ColeRutledge@gmail.com>
2021-03-16 11:00:33 -0400

    adds requests to workflow

--- docker/release.py
+++ docker/release.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python3
 
-from urllib import request, parse, error
+# from urllib import request, parse, error
+import requests
 
 import os
 import subprocess
@@ -13,26 +14,30 @@ WEB_DOCKER_IMAGE_ID = subprocess.run([
     '--format={{.Id}}'], capture_output=True, encoding='utf-8').stdout.strip()
 
 data = {"updates": [{"type": "web", "docker_image": f"{WEB_DOCKER_IMAGE_ID}"}]}
-data = parse.urlencode(data)
-data = data.encode('ascii')
+# data = parse.urlencode(data)
+# data = data.encode('ascii')
 headers = {
     "Content-Type": "application/json",
     "Accept": "application/vnd.heroku+json; version=3.docker-releases",
     "Authorization": f"Bearer {HEROKU_AUTH_TOKEN}"}
 
-res = request.Request(
-    url="https://api.heroku.com/apps/asana-fastapi/formation",
+
+r = requests.patch(
     data=data,
-    method='PATCH',
-    headers=headers)
+    headers=headers,
+    url="https://api.heroku.com/apps/asana-fastapi/formation")
 
-try:
-    with request.urlopen(res) as response:
-        html = response.read().decode('utf-8')
-        print(html)
-except error.HTTPError as e:
-    print(e.reason)
+print(r)
 
-print(res)
-print(vars(res))
-print(dir(res))
+# res = request.Request(
+#     url="https://api.heroku.com/apps/asana-fastapi/formation",
+#     data=data,
+#     method='PATCH',
+#     headers=headers)
+
+# try:
+#     with request.urlopen(res) as response:
+#         html = response.read().decode('utf-8')
+#         print(html)
+# except error.HTTPError as e:
+#     print(e.reason)
--- docker/release.sh
+++ docker/release.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-IMAGE_ID=$(docker inspect registry.heroku.com/asana-fastapi/web:latest --format={{.Id}})
+IMAGE_ID=$(docker inspect registry.heroku.com/asana-fastapi/web --format={{.Id}})
 PAYLOAD='{"updates": [{"type": "web", "docker_image": "'"$IMAGE_ID"'"}]}'
 
 curl -n -X PATCH https://api.heroku.com/apps/asana-fastapi/formation \
