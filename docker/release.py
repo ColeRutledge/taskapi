651222ce767a25ffadbbbc6075a92c66a226236a
Cole Rutledge <ColeRutledge@gmail.com>
2021-03-16 11:58:24 -0400

    remove cache and buildx setup in cd

--- docker/release.py
+++ docker/release.py
@@ -1,10 +1,12 @@
 #! /usr/bin/env python3
 
+from urllib import request, parse, error
+
 import json
 import os
 import subprocess
 
-import requests
+# import requests
 
 
 HEROKU_AUTH_TOKEN = os.environ.get('HEROKU_AUTH_TOKEN')
@@ -15,33 +17,28 @@ WEB_DOCKER_IMAGE_ID = subprocess.run([
     '--format={{.Id}}'], capture_output=True, encoding='utf-8').stdout.strip()
 
 data = {"updates": [{"type": "web", "docker_image": f"{WEB_DOCKER_IMAGE_ID}"}]}
-# data = parse.urlencode(data)
-# data = data.encode('ascii')
+data = parse.urlencode(json.dumps(data))
+data = data.encode('ascii')
 headers = {
     "Content-Type": "application/json",
     "Accept": "application/vnd.heroku+json; version=3.docker-releases",
     "Authorization": f"Bearer {HEROKU_AUTH_TOKEN}"}
 
 
-r = requests.patch(
-    data=json.dumps(data),
-    headers=headers,
-    url="https://api.heroku.com/apps/asana-fastapi/formation")
+# r = requests.patch(
+#     data=json.dumps(data),
+#     headers=headers,
+#     url="https://api.heroku.com/apps/asana-fastapi/formation")
 
-print(r.reason)
-print(r)
-print(vars(r))
-print(dir(r))
+res = request.Request(
+    url="https://api.heroku.com/apps/asana-fastapi/formation",
+    data=data,
+    method='PATCH',
+    headers=headers)
 
-# res = request.Request(
-#     url="https://api.heroku.com/apps/asana-fastapi/formation",
-#     data=data,
-#     method='PATCH',
-#     headers=headers)
-
-# try:
-#     with request.urlopen(res) as response:
-#         html = response.read().decode('utf-8')
-#         print(html)
-# except error.HTTPError as e:
-#     print(e.reason)
+try:
+    with request.urlopen(res) as response:
+        html = response.read()  # .decode('utf-8')
+        print(html)
+except error.HTTPError as e:
+    print(e.reason)
