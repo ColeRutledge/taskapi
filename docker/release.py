516e4d73bc22850db3716a755c65c91c3c4a3bbd
Cole Rutledge <ColeRutledge@gmail.com>
2021-03-16 15:53:53 -0400

    formatting

--- docker/release.py
+++ docker/release.py
@@ -7,24 +7,22 @@ from urllib import request, error
 
 HEROKU_APP_NAME = os.environ.get('HEROKU_APP_NAME')
 HEROKU_AUTH_TOKEN = os.environ.get('HEROKU_AUTH_TOKEN')
-WEB_DOCKER_IMAGE_ID = subprocess.run([
+IMAGE_ID = subprocess.run([
     'docker',
     'inspect',
     f'registry.heroku.com/{HEROKU_APP_NAME}/web:latest',
     '--format={{.Id}}'], capture_output=True, encoding='utf-8').stdout.strip()
 
-data = {
-    "updates": [{"type": "web", "docker_image": f"{WEB_DOCKER_IMAGE_ID}"}]}
+payload = {"updates": [{"type": "web", "docker_image": f"{IMAGE_ID}"}]}
+payload = json.dumps(payload).encode('utf-8')
 headers = {
     "Content-Type": "application/json",
     "Accept": "application/vnd.heroku+json; version=3.docker-releases",
     "Authorization": f"Bearer {HEROKU_AUTH_TOKEN}"}
 
 r = request.Request(
-    url=f"https://api.heroku.com/apps/{HEROKU_APP_NAME}/formation",
-    data=json.dumps(data).encode('utf-8'),
-    method='PATCH',
-    headers=headers)
+    method='PATCH', headers=headers, data=payload,
+    url=f"https://api.heroku.com/apps/{HEROKU_APP_NAME}/formation")
 
 try:
     with request.urlopen(r) as response:
