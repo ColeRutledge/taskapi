65274af2555ce6a132d110f707a12085f2bb4c2f
Cole Rutledge <ColeRutledge@gmail.com>
2021-03-16 10:26:58 -0400

    round 3

--- docker/release.py
+++ docker/release.py
@@ -12,9 +12,9 @@ WEB_DOCKER_IMAGE_ID = subprocess.run([
     'registry.heroku.com/asana-fastapi/web:latest',
     '--format={{.Id}}'], capture_output=True, encoding='utf-8').stdout
 
-data = {"updates": [{"type": "web", "docker_image": f"{WEB_DOCKER_IMAGE_ID}"}]}
-data = parse.urlencode(data)
-data = data.encode('ascii')
+# data = {"updates": [{"type": "web", "docker_image": f"{WEB_DOCKER_IMAGE_ID}"}]}
+# data = parse.urlencode(data)
+# data = data.encode('ascii')
 headers = {
     "Content-Type": "application/json",
     "Accept": "application/vnd.heroku+json; version=3.docker-releases",
@@ -22,7 +22,7 @@ headers = {
 
 res = request.Request(
     url="https://api.heroku.com/apps/asana-fastapi/formation",
-    data=data,
+    # data=data,
     method='PATCH',
     headers=headers)
 
