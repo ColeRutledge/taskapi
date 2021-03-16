b3e5ac5fede17cc04e74002a264ebdefa74905f3
Cole Rutledge <ColeRutledge@gmail.com>
2021-03-16 12:02:22 -0400

    encode data with json

--- docker/release.py
+++ docker/release.py
@@ -17,8 +17,8 @@ WEB_DOCKER_IMAGE_ID = subprocess.run([
     '--format={{.Id}}'], capture_output=True, encoding='utf-8').stdout.strip()
 
 data = {"updates": [{"type": "web", "docker_image": f"{WEB_DOCKER_IMAGE_ID}"}]}
-data = parse.urlencode(json.dumps(data))
-data = data.encode('ascii')
+# data = parse.urlencode(json.dumps(data))
+# data = data.encode('ascii')
 headers = {
     "Content-Type": "application/json",
     "Accept": "application/vnd.heroku+json; version=3.docker-releases",
@@ -32,7 +32,7 @@ headers = {
 
 res = request.Request(
     url="https://api.heroku.com/apps/asana-fastapi/formation",
-    data=data,
+    data=json.dumps(data),
     method='PATCH',
     headers=headers)
 
