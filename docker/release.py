0125cad93ba19c58eb44bbe96061dac7cb14b289
Cole Rutledge <ColeRutledge@gmail.com>
2021-03-16 15:20:24 -0400

    final update with env vars

--- docker/release.py
+++ docker/release.py
@@ -5,12 +5,12 @@ import os
 import subprocess
 from urllib import request, error
 
-
+HEROKU_APP_NAME = os.environ.get('HEROKU_APP_NAME')
 HEROKU_AUTH_TOKEN = os.environ.get('HEROKU_AUTH_TOKEN')
 WEB_DOCKER_IMAGE_ID = subprocess.run([
     'docker',
     'inspect',
-    'registry.heroku.com/asana-fastapi/web:latest',
+    f'registry.heroku.com/{HEROKU_APP_NAME}/web:latest',
     '--format={{.Id}}'], capture_output=True, encoding='utf-8').stdout.strip()
 
 data = {
@@ -21,7 +21,7 @@ headers = {
     "Authorization": f"Bearer {HEROKU_AUTH_TOKEN}"}
 
 r = request.Request(
-    url="https://api.heroku.com/apps/asana-fastapi/formation",
+    url=f"https://api.heroku.com/apps/{HEROKU_APP_NAME}/formation",
     data=json.dumps(data).encode('utf-8'),
     method='PATCH',
     headers=headers)
