a44e2bc16bd87f1b943e75ba7e2036ebf64ea795
Cole Rutledge <ColeRutledge@gmail.com>
2021-03-16 10:44:14 -0400

    adds strip

--- docker/release.py
+++ docker/release.py
@@ -10,7 +10,7 @@ WEB_DOCKER_IMAGE_ID = subprocess.run([
     'docker',
     'inspect',
     'registry.heroku.com/asana-fastapi/web:latest',
-    '--format={{.Id}}'], capture_output=True, encoding='utf-8').stdout
+    '--format={{.Id}}'], capture_output=True, encoding='utf-8').stdout.strip()
 
 data = {"updates": [{"type": "web", "docker_image": f"{WEB_DOCKER_IMAGE_ID}"}]}
 data = parse.urlencode(data)
@@ -20,13 +20,11 @@ headers = {
     "Accept": "application/vnd.heroku+json; version=3.docker-releases",
     "Authorization": f"Bearer {HEROKU_AUTH_TOKEN}"}
 
-res = request.Request(url="https://api.heroku.com/apps/asana-fastapi/formation")
-
-# res = request.Request(
-#     url="https://api.heroku.com/apps/asana-fastapi/formation",
-#     data=data,
-#     method='PATCH',
-#     headers=headers)
+res = request.Request(
+    url="https://api.heroku.com/apps/asana-fastapi/formation",
+    data=data,
+    method='PATCH',
+    headers=headers)
 
 try:
     with request.urlopen(res) as response:
