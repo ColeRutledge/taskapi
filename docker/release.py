20d6f22227ce70a6782baf37c0a83a0ae28aeb52
Cole Rutledge <ColeRutledge@gmail.com>
2021-03-16 21:41:36 -0400

    formatting ✨

--- docker/release.py
+++ docker/release.py
@@ -7,22 +7,24 @@ from urllib import request, error
 
 HEROKU_APP_NAME = os.environ.get('HEROKU_APP_NAME')
 HEROKU_AUTH_TOKEN = os.environ.get('HEROKU_AUTH_TOKEN')
-IMAGE_ID = subprocess.run([
-    'docker',
-    'inspect',
-    f'registry.heroku.com/{HEROKU_APP_NAME}/web:latest',
-    '--format={{.Id}}'], capture_output=True, encoding='utf-8').stdout.strip()
+IMAGE_ID = subprocess.run(
+    args=['docker',
+          'inspect',
+          f'registry.heroku.com/{HEROKU_APP_NAME}/web:latest',
+          '--format={{.Id}}'],
+    capture_output=True,
+    encoding='utf-8').stdout.strip()
 
-payload = {"updates": [{"type": "web", "docker_image": f"{IMAGE_ID}"}]}
+payload = {'updates': [{'type': 'web', 'docker_image': f'{IMAGE_ID}'}]}
 payload = json.dumps(payload).encode('utf-8')
 headers = {
-    "Content-Type": "application/json",
-    "Accept": "application/vnd.heroku+json; version=3.docker-releases",
-    "Authorization": f"Bearer {HEROKU_AUTH_TOKEN}"}
+    'Content-Type': 'application/json',
+    'Accept': 'application/vnd.heroku+json; version=3.docker-releases',
+    'Authorization': f'Bearer {HEROKU_AUTH_TOKEN}'}
 
 r = request.Request(
     method='PATCH', headers=headers, data=payload,
-    url=f"https://api.heroku.com/apps/{HEROKU_APP_NAME}/formation")
+    url=f'https://api.heroku.com/apps/{HEROKU_APP_NAME}/formation')
 
 try:
     with request.urlopen(r) as response:
