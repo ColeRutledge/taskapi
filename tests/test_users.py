8b002716b19f1a68a45fd69879d6b03774ad30c7
Cole Rutledge <ColeRutledge@gmail.com>
2021-03-17 17:51:11 -0400

    test_get_all_users - variable change:mocked_response -> response_data :white_check_mark:

--- tests/test_users.py
+++ tests/test_users.py
@@ -11,12 +11,12 @@ def test_get_all_users(
     for user_schema in three_user_schemas:
         crud.create_user(test_db, user_schema)
     response = test_app.get('/users/')
-    mocked_response = response.json()
-    mocked_response[0]['hashed_password'] = 'mock_hashed_password'
-    mocked_response[1]['hashed_password'] = 'mock_hashed_password'
-    mocked_response[2]['hashed_password'] = 'mock_hashed_password'
+    response_data = response.json()
+    response_data[0]['hashed_password'] = 'mock_hashed_password'
+    response_data[1]['hashed_password'] = 'mock_hashed_password'
+    response_data[2]['hashed_password'] = 'mock_hashed_password'
     assert response.status_code == 200
-    assert mocked_response == [
+    assert response_data == [
         {'first_name': 'test1', 'last_name': 'user1', 'email': 'test1@user.com', 'id': 1,
          'hashed_password': 'mock_hashed_password', 'team_id': None, 'disabled': None},
         {'first_name': 'test2', 'last_name': 'user2', 'email': 'test2@user.com', 'id': 2,
