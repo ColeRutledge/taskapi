88ab90a0c118b2924059409f73ea2371f8bc0c18
Cole Rutledge <ColeRutledge@gmail.com>
2021-03-18 18:54:13 -0400

    update tests to match seed data :white_check_mark:

--- tests/test_users.py
+++ tests/test_users.py
@@ -7,21 +7,20 @@ def test_get_all_users(test_app: TestClient, test_db_seeded: Session):
     response_data = response.json()
     assert response.status_code == 200
     assert response_data == [
-        {'first_name': 'test1', 'last_name': 'user1', 'email': 'test1@user.com',
+        {'first_name': 'Bob', 'last_name': 'Smith', 'email': 'bob@smith.com',
          'id': 1, 'team_id': 1, 'disabled': None},
-        {'first_name': 'test2', 'last_name': 'user2', 'email': 'test2@user.com',
+        {'first_name': 'Sally', 'last_name': 'McBeth', 'email': 'Sally@123.com',
          'id': 2, 'team_id': 1, 'disabled': None},
-        {'first_name': 'test3', 'last_name': 'user3', 'email': 'test3@user.com',
+        {'first_name': 'John', 'last_name': 'Applebaum', 'email': 'johnnyA@user.com',
          'id': 3, 'team_id': 1, 'disabled': None},
-        {'first_name': 'test4', 'last_name': 'user4', 'email': 'test4@user.com',
+        {'first_name': 'Bill', 'last_name': 'McSorley', 'email': 'billy-max@rocks.com',
          'id': 4, 'team_id': 2, 'disabled': None}]
 
 
 def test_get_user(test_app: TestClient, test_db_seeded: Session):
     user_id = 1
     response = test_app.get(f'/users/{user_id}')
-    print(vars(response))
     assert response.status_code == 200
     assert response.json() == {
-        'first_name': 'test1', 'last_name': 'user1', 'email': 'test1@user.com',
+        'first_name': 'Bob', 'last_name': 'Smith', 'email': 'bob@smith.com',
         'id': 1, 'team_id': 1, 'disabled': None}
