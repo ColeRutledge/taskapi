c535f9a4278f12a7034d20dc638d50ec96333170
Cole Rutledge <ColeRutledge@gmail.com>
2021-03-19 11:49:45 -0400

    add parametrized users tests :white_check_mark:

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,3 +1,5 @@
+import pytest
+
 from fastapi.testclient import TestClient
 from sqlalchemy.orm import Session
 
@@ -17,10 +19,32 @@ def test_get_all_users(test_app: TestClient, test_db_seeded: Session):
          'id': 4, 'team_id': 2, 'disabled': None}]
 
 
-def test_get_user(test_app: TestClient, test_db_seeded: Session):
-    user_id = 1
+@pytest.mark.parametrize(argnames='user_id', argvalues=[1, 2, 3, 4])
+def test_get_user(user_id: int, test_app: TestClient, test_db_seeded: Session):
     response = test_app.get(f'/users/{user_id}')
     assert response.status_code == 200
-    assert response.json() == {
-        'first_name': 'Bob', 'last_name': 'Smith', 'email': 'bob@smith.com',
-        'id': 1, 'team_id': 1, 'disabled': None}
+    assert response.json() in [
+        {'first_name': 'Bob', 'last_name': 'Smith', 'email': 'bob@smith.com',
+         'id': 1, 'team_id': 1, 'disabled': None},
+        {'first_name': 'Sally', 'last_name': 'McBeth', 'email': 'Sally@123.com',
+         'id': 2, 'team_id': 1, 'disabled': None},
+        {'first_name': 'John', 'last_name': 'Applebaum', 'email': 'johnnyA@user.com',
+         'id': 3, 'team_id': 1, 'disabled': None},
+        {'first_name': 'Bill', 'last_name': 'McSorley', 'email': 'billy-max@rocks.com',
+         'id': 4, 'team_id': 2, 'disabled': None}]
+
+
+def test_get_user_not_found(test_app: TestClient, test_db_seeded: Session):
+    invalid_id = 0
+    response = test_app.get(f'/users/{invalid_id}')
+    assert response.status_code == 404
+    assert response.json()['detail'] == 'User not found'
+
+
+@pytest.mark.parametrize(argnames='user_id', argvalues=[1, 2, 3, 4])
+def test_get_user_team(user_id: int, test_app: TestClient, test_db_seeded: Session):
+    response = test_app.get(f'/users/{user_id}/team')
+    assert response.status_code == 200
+    assert response.json() in [
+        {'id': 1, 'team_name': 'Marketing'},
+        {'id': 2, 'team_name': 'Engineering'}]
