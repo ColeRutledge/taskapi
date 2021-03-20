4f40120359d79b04d676aa4315621bbaea9c50ee
Cole Rutledge <ColeRutledge@gmail.com>
2021-03-20 19:22:40 -0400

    :package: confirm user identity in update route & logging

--- app/routers/users.py
+++ app/routers/users.py
@@ -1,3 +1,5 @@
+import logging
+
 from fastapi import APIRouter, Depends, HTTPException, Body, status
 from sqlalchemy.orm.session import Session
 
@@ -19,9 +21,8 @@ def get_user(user_id: int, db: Session = Depends(get_db)):
     db_user: models.User = crud.read(db=db, id=user_id, model=models.User)
     if db_user is None:
         raise HTTPException(
-            status_code=404,
-            detail='User not found',
-        )
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail='User not found')
     return db_user
 
 
@@ -29,25 +30,28 @@ def get_user(user_id: int, db: Session = Depends(get_db)):
 def get_user_team(user_id: int, db: Session = Depends(get_db)):
     db_user: models.User = crud.read(db=db, id=user_id, model=models.User)
     if db_user is None:
-        raise HTTPException(
-            status_code=404,
-            detail='User not found',
-        )
+        raise HTTPException(status_code=404, detail='User not found')
     return db_user.team
 
 
-@router.put('/{user_id}', response_model=schemas.User, dependencies=[Depends(gcu)])
+@router.put('/{user_id}', response_model=schemas.User)
 def update_user(
-    user_id: int,
-    user: schemas.UserUpdate = Body(..., embed=True),
-    db: Session = Depends(get_db),
-):
+        user_id: int,
+        user: schemas.UserUpdate = Body(..., embed=True),
+        current_user: models.User = Depends(gcu),
+        db: Session = Depends(get_db)):
     db_user: models.User = crud.read(db=db, id=user_id, model=models.User)
     if db_user is None:
         raise HTTPException(
-            status_code=404,
-            detail='User not found',
-        )
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail='User not found')
+    elif current_user != db_user:
+        logging.warning(
+            f'User[{current_user.email}] tried to access User[{db_user.email}]')
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail='Could not validate credentials',
+            headers={'WWW-Authenticate': 'Bearer'})
     return crud.update_user(db, schema=user, model=db_user)
 
 
@@ -56,9 +60,8 @@ def delete_user(user_id: int, db: Session = Depends(get_db)):
     db_user: models.User = crud.read(db=db, id=user_id, model=models.User)
     if db_user is None:
         raise HTTPException(
-            status_code=404,
-            detail='User not found',
-        )
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail='User not found')
     return crud.delete(db=db, resource=db_user)
 
 
@@ -66,5 +69,7 @@ def delete_user(user_id: int, db: Session = Depends(get_db)):
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     db_user = models.User.get_user_by_email(db, email=user.email)
     if db_user:
-        raise HTTPException(status_code=400, detail="Email already registered")
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Email already registered")
     return crud.create_user(db=db, user=user)
