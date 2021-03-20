07b257c6eaa8e6df819e67670709d40677a107f6
Cole Rutledge <ColeRutledge@gmail.com>
2021-03-20 19:27:47 -0400

    :white_check_mark: add robustness to exception testing

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,15 +1,15 @@
 import json
 
 import pytest
+from fastapi import status
 from fastapi.testclient import TestClient
 from sqlalchemy.orm import Session
 
 
 def test_get_all_users(test_app: TestClient, test_db_seeded: Session):
     response = test_app.get('/users/')
-    response_data = response.json()
-    assert response.status_code == 200
-    assert response_data == [
+    assert response.status_code == status.HTTP_200_OK
+    assert response.json() == [
         {'first_name': 'Bob', 'last_name': 'Smith', 'email': 'bob@smith.com',
          'id': 1, 'team_id': 1, 'disabled': None},
         {'first_name': 'Sally', 'last_name': 'McBeth', 'email': 'Sally@123.com',
@@ -18,61 +18,78 @@ def test_get_all_users(test_app: TestClient, test_db_seeded: Session):
          'id': 3, 'team_id': 1, 'disabled': None},
         {'first_name': 'Bill', 'last_name': 'McSorley', 'email': 'billy-max@rocks.com',
          'id': 4, 'team_id': 2, 'disabled': None}]
-
-
-@pytest.mark.parametrize(argnames='user_id', argvalues=[1, 2, 3, 4])
-def test_get_user(user_id: int, test_app: TestClient, test_db_seeded: Session):
-    response = test_app.get(f'/users/{user_id}')
-    assert response.status_code == 200
-    assert response.json() in [
-        {'first_name': 'Bob', 'last_name': 'Smith', 'email': 'bob@smith.com',
-         'id': 1, 'team_id': 1, 'disabled': None},
-        {'first_name': 'Sally', 'last_name': 'McBeth', 'email': 'Sally@123.com',
-         'id': 2, 'team_id': 1, 'disabled': None},
-        {'first_name': 'John', 'last_name': 'Applebaum', 'email': 'johnnyA@user.com',
-         'id': 3, 'team_id': 1, 'disabled': None},
-        {'first_name': 'Bill', 'last_name': 'McSorley', 'email': 'billy-max@rocks.com',
-         'id': 4, 'team_id': 2, 'disabled': None}]
-
-
-def test_get_user_invalid(test_app: TestClient, test_db_seeded: Session):
-    invalid_user_id = 0
-    response = test_app.get(f'/users/{invalid_user_id}')
-    assert response.status_code == 404
-    assert response.json()['detail'] == 'User not found'
-
-
-@pytest.mark.parametrize(argnames='user_id', argvalues=[1, 2, 3, 4])
-def test_get_user_team(user_id: int, test_app: TestClient, test_db_seeded: Session):
-    response = test_app.get(f'/users/{user_id}/team')
-    assert response.status_code == 200
-    assert response.json() in [
-        {'id': 1, 'team_name': 'Marketing'},
-        {'id': 2, 'team_name': 'Engineering'}]
-
-
-def test_get_user_team_invalid(test_app: TestClient, test_db_seeded: Session):
-    invalid_user_id = 0
-    response = test_app.get(f'/users/{invalid_user_id}/team')
-    assert response.status_code == 404
-    assert response.json()['detail'] == 'User not found'
 
 
 @pytest.mark.parametrize(
-    argnames=['user_id', 'first_name', 'email'],
+    argnames=['user_id', 'status_code', 'expected_response'],
     argvalues=[
-        (1, 'John', 'john@smith.com'),
-        (2, 'Betty', 'Betty@xyz.com'),
-        (3, 'Rohan', 'rohan@user.com'),
-        (4, 'Rob', 'robby@smith.com')])
-def test_update_user(
+        (1, status.HTTP_200_OK, {
+            'first_name': 'Bob', 'last_name': 'Smith', 'email': 'bob@smith.com',
+            'id': 1, 'team_id': 1, 'disabled': None}),
+        (2, status.HTTP_200_OK, {
+            'first_name': 'Sally', 'last_name': 'McBeth', 'email': 'Sally@123.com',
+            'id': 2, 'team_id': 1, 'disabled': None}),
+        (4, status.HTTP_200_OK, {
+            'first_name': 'Bill', 'last_name': 'McSorley', 'email': 'billy-max@rocks.com',
+            'id': 4, 'team_id': 2, 'disabled': None}),
+        (0, status.HTTP_404_NOT_FOUND, {'detail': 'User not found'})])
+def test_get_user(
         user_id: int,
-        first_name: str,
-        email: str,
+        status_code: int,
+        expected_response: dict,
         test_app: TestClient,
         test_db_seeded: Session):
-    payload = json.dumps({'user': {'first_name': first_name, 'email': email}})
-    put_response = test_app.put(f'/users/{user_id}', data=payload)
-    get_response = test_app.get(f'/users/{user_id}')
-    assert put_response.status_code == 200
-    assert put_response.json() == get_response.json()
+    response = test_app.get(f'/users/{user_id}')
+    assert response.status_code == status_code
+    assert response.json() == expected_response
+
+
+@pytest.mark.parametrize(
+    argnames=['user_id', 'status_code', 'expected_response'],
+    argvalues=[
+        (1, status.HTTP_200_OK, {'id': 1, 'team_name': 'Marketing'}),
+        (2, status.HTTP_200_OK, {'id': 1, 'team_name': 'Marketing'}),
+        (4, status.HTTP_200_OK, {'id': 2, 'team_name': 'Engineering'}),
+        (0, status.HTTP_404_NOT_FOUND, {'detail': 'User not found'}),
+        ('bad_user_id', status.HTTP_422_UNPROCESSABLE_ENTITY, {
+            'detail': [{
+                'loc': ['path', 'user_id'],
+                'msg': 'value is not a valid integer',
+                'type': 'type_error.integer'}]})])
+def test_get_user_team(
+        user_id: int,
+        status_code: int,
+        expected_response: dict,
+        test_app: TestClient,
+        test_db_seeded: Session):
+    response = test_app.get(f'/users/{user_id}/team')
+    assert response.status_code == status_code
+    assert response.json() == expected_response
+
+
+@pytest.mark.parametrize(
+    argnames=['user_id', 'field', 'value', 'status_code', 'expected_response'],
+    argvalues=[
+        (1, 'first_name', 'John', status.HTTP_200_OK, {
+            'first_name': 'John', 'last_name': 'Smith', 'email': 'bob@smith.com',
+            'id': 1, 'team_id': 1, 'disabled': None}),
+        (2, 'email', 'Betty@xyz.com', status.HTTP_401_UNAUTHORIZED, {
+            'detail': 'Could not validate credentials'}),
+        (0, 'team_id', 1, status.HTTP_404_NOT_FOUND, {'detail': 'User not found'}),
+        ('bad_user_id', 'team_id', 1, status.HTTP_422_UNPROCESSABLE_ENTITY, {
+            'detail': [{
+                'loc': ['path', 'user_id'],
+                'msg': 'value is not a valid integer',
+                'type': 'type_error.integer'}]})])
+def test_update_user(
+        user_id: int,
+        field: str,
+        value: str,
+        status_code: int,
+        expected_response: dict,
+        test_app: TestClient,
+        test_db_seeded: Session):
+    payload = json.dumps({'user': {field: value}})
+    response = test_app.put(f'/users/{user_id}', data=payload)
+    assert response.status_code == status_code
+    assert response.json() == expected_response
