7cb3f31e488f52e5bfc001efd579746733608533
Cole Rutledge <ColeRutledge@gmail.com>
2021-03-19 23:20:27 -0400

    config logging and db setup on entry :rocket:

--- asana_fastapi.py
+++ asana_fastapi.py
@@ -1,7 +1,22 @@
+import os
 import uvicorn
+from logging.config import dictConfig
+
+from sqlalchemy.orm import Session
+
+from app import db, models, config
+from migrations.seed import seed_db
 
 
 if __name__ == '__main__':
+    os.makedirs('logs', exist_ok=True)
+    dictConfig(config.LOGGING_CONFIG)
+
+    models.Base.metadata.create_all(bind=db.engine, checkfirst=True)
+    session = Session(autocommit=False, autoflush=False, bind=db.engine)
+    seed_db(session)
+    session.close()
+
     uvicorn.run(
         'app.main:app',
         host="0.0.0.0",
