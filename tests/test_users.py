2187f47dce8b5adc24170295f33cf00dda615380
Cole Rutledge <ColeRutledge@gmail.com>
2021-03-19 20:04:30 -0400

    add test_update_user :white_check_mark:

--- tests/test_users.py
+++ tests/test_users.py
@@ -1,5 +1,6 @@
-import pytest
+import json
 
+import pytest
 from fastapi.testclient import TestClient
 from sqlalchemy.orm import Session
 
@@ -34,9 +35,9 @@ def test_get_user(user_id: int, test_app: TestClient, test_db_seeded: Session):
          'id': 4, 'team_id': 2, 'disabled': None}]
 
 
-def test_get_user_not_found(test_app: TestClient, test_db_seeded: Session):
-    invalid_id = 0
-    response = test_app.get(f'/users/{invalid_id}')
+def test_get_user_invalid(test_app: TestClient, test_db_seeded: Session):
+    invalid_user_id = 0
+    response = test_app.get(f'/users/{invalid_user_id}')
     assert response.status_code == 404
     assert response.json()['detail'] == 'User not found'
 
@@ -50,8 +51,25 @@ def test_get_user_team(user_id: int, test_app: TestClient, test_db_seeded: Sessi
         {'id': 2, 'team_name': 'Engineering'}]
 
 
-def test_get_user_team_not_found(test_app: TestClient, test_db_seeded: Session):
-    invalid_id = 0
-    response = test_app.get(f'/users/{invalid_id}/team')
+def test_get_user_team_invalid(test_app: TestClient, test_db_seeded: Session):
+    invalid_user_id = 0
+    response = test_app.get(f'/users/{invalid_user_id}/team')
     assert response.status_code == 404
     assert response.json()['detail'] == 'User not found'
+
+
+def test_update_user(test_app: TestClient, test_db_seeded: Session):
+    user_id = 1
+    put_response = test_app.put(
+        f'/users/{user_id}',
+        data=json.dumps({
+            'user': {
+                'first_name': 'John',       # updating from 'Bob'
+                'last_name': 'Smith',
+                'email': 'john@smith.com',  # updating from 'bob@smith.com'
+                'password': 'password',
+                'team_id': 1,
+                'disabled': False}}))
+    get_response = test_app.get(f'/users/{user_id}')
+    assert put_response.status_code == 200
+    assert put_response.json() == get_response.json()
