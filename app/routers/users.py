99901ad64e936584acf37fc44224e84df24ca1eb
Cole Rutledge <ColeRutledge@gmail.com>
2021-03-20 21:39:09 -0400

    :fire: remove keyword args for clarity

--- app/routers/users.py
+++ app/routers/users.py
@@ -13,12 +13,12 @@ router = APIRouter()
 
 @router.get('/', response_model=list[schemas.User], dependencies=[Depends(gcu)])
 def get_all_users(db: Session = Depends(get_db)):
-    return crud.read_all(db=db, model=models.User)
+    return crud.read_all(db, models.User)
 
 
 @router.get('/{user_id}', response_model=schemas.User, dependencies=[Depends(gcu)])
 def get_user(user_id: int, db: Session = Depends(get_db)):
-    db_user: models.User = crud.read(db=db, id=user_id, model=models.User)
+    db_user: models.User = crud.read(db, user_id, models.User)
     if db_user is None:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
@@ -28,7 +28,7 @@ def get_user(user_id: int, db: Session = Depends(get_db)):
 
 @router.get('/{user_id}/team', response_model=schemas.Team, dependencies=[Depends(gcu)])
 def get_user_team(user_id: int, db: Session = Depends(get_db)):
-    db_user: models.User = crud.read(db=db, id=user_id, model=models.User)
+    db_user: models.User = crud.read(db, user_id, models.User)
     if db_user is None:
         raise HTTPException(status_code=404, detail='User not found')
     return db_user.team
@@ -37,22 +37,22 @@ def get_user_team(user_id: int, db: Session = Depends(get_db)):
 @router.put('/{user_id}', response_model=schemas.User)
 def update_user(
         user_id: int,
-        user: schemas.UserUpdate = Body(..., embed=True),
+        user_schema: schemas.UserUpdate = Body(..., embed=True),
         current_user: models.User = Depends(gcu),
         db: Session = Depends(get_db)):
-    db_user: models.User = crud.read(db=db, id=user_id, model=models.User)
+    db_user: models.User = crud.read(db, user_id, models.User)
     if db_user is None:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
             detail='User not found')
     elif current_user != db_user:
         logging.warning(
-            f'User[{current_user.email}] tried to access User[{db_user.email}]')
+            f'User[{current_user.email}] tried to update User[{db_user.email}]')
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail='Could not validate credentials',
             headers={'WWW-Authenticate': 'Bearer'})
-    return crud.update_user(db, schema=user, model=db_user)
+    return crud.update_user(db, user_schema, db_user)
 
 
 @router.delete('/{user_id}', response_model=schemas.User)
@@ -60,26 +60,26 @@ def delete_user(
         user_id: int,
         current_user: models.User = Depends(gcu),
         db: Session = Depends(get_db)):
-    db_user: models.User = crud.read(db=db, id=user_id, model=models.User)
+    db_user: models.User = crud.read(db, user_id, models.User)
     if db_user is None:
         raise HTTPException(
             status_code=status.HTTP_404_NOT_FOUND,
             detail='User not found')
     elif current_user != db_user:
         logging.warning(
-            f'User[{current_user.email}] tried to access User[{db_user.email}]')
+            f'User[{current_user.email}] tried to delete User[{db_user.email}]')
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail='Could not validate credentials',
             headers={'WWW-Authenticate': 'Bearer'})
-    return crud.delete(db=db, resource=db_user)
+    return crud.delete(db, db_user)
 
 
 @router.post('/', response_model=schemas.User, status_code=status.HTTP_201_CREATED)
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
-    db_user = models.User.get_user_by_email(db, email=user.email)
+    db_user = models.User.get_user_by_email(db, user.email)
     if db_user:
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
             detail="Email already registered")
-    return crud.create_user(db=db, user=user)
+    return crud.create_user(db, user)
