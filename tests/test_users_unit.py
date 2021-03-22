667abd35f1653d390a2dbb8d4b1c1fdf65e3709f
Cole Rutledge <ColeRutledge@gmail.com>
2021-03-22 14:53:27 -0400

    ✨ rework unittest params to be more flexible

--- tests/test_users_unit.py
+++ tests/test_users_unit.py
@@ -18,68 +18,74 @@ def test_get_all_users(monkeypatch, test_app: TestClient):
 
     def mock_read_all(db, user_model):
         mock_user = models.User(
-            id=1, team_id=1, first_name='Bob',
-            last_name='Smith', email='bob@smith.com')
+            id=1, team_id=1, first_name='Test',
+            last_name='User', email='test@user.com')
         return [mock_user]
 
     monkeypatch.setattr(crud, 'read_all', mock_read_all)
     response = test_app.get('/users/')
     assert response.status_code == HTTP_200_OK
     assert response.json() == [{
-        'first_name': 'Bob', 'last_name': 'Smith', 'id': 1,
-        'email': 'bob@smith.com', 'team_id': 1, 'disabled': None}]
+        'first_name': 'Test', 'last_name': 'User', 'id': 1,
+        'email': 'test@user.com', 'team_id': 1, 'disabled': None}]
 
 
 @pytest.mark.parametrize(
-    argnames=['user_id', 'mock_user', 'status_code', 'expected_response'],
+    argnames=['user_id', 'status_code', 'field', 'value'],
     argvalues=[
-        (1, models.User(
-            id=1, team_id=1, first_name='Bob',
-            last_name='Smith', email='bob@smith.com'), HTTP_200_OK, {
-                'first_name': 'Bob', 'last_name': 'Smith', 'id': 1,
-                'email': 'bob@smith.com', 'team_id': 1, 'disabled': None}),
-        (0, None, HTTP_404_NOT_FOUND, {'detail': 'User not found'})])
+        (1, HTTP_200_OK, 'email', 'expected@email.com'),
+        (0, HTTP_404_NOT_FOUND, 'detail', 'User not found')])
 def test_get_user(
         user_id: int,
-        mock_user: models.User,
         status_code: int,
-        expected_response: dict,
+        field: Union[str, int],
+        value: Union[str, int],
         monkeypatch,
         test_app: TestClient):
 
+    mock_user = models.User(
+        id=user_id,
+        team_id=1,
+        first_name='fill',
+        last_name='fill',
+        email='expected@email.com')
+
     def mock_read(db, user_id, user_model):
+        if user_id == 0:  # user that does not exist
+            return None
         return mock_user
 
     monkeypatch.setattr(crud, 'read', mock_read)
     response = test_app.get(f'/users/{user_id}')
     assert response.status_code == status_code
-    assert response.json() == expected_response
+    assert response.json()[field] == value
 
 
 @pytest.mark.parametrize(
-    argnames=['user_id', 'mock_team', 'status_code', 'expected_response'],
+    argnames=['user_id', 'status_code', 'field', 'value'],
     argvalues=[
-        (1, {'id': 1, 'team_name': 'Marketing'}, HTTP_200_OK, {
-            'id': 1, 'team_name': 'Marketing'}),
-        (0, None, HTTP_404_NOT_FOUND, {'detail': 'User not found'})])
+        (1, HTTP_200_OK, 'team_name', 'Testing'),
+        (0, HTTP_404_NOT_FOUND, 'detail', 'User not found')])
 def test_get_user_team(
         user_id: int,
-        mock_team: models.Team,
         status_code: int,
-        expected_response: dict,
+        field: Union[int, str],
+        value: Union[int, str],
         monkeypatch,
         test_app: TestClient):
 
+    mockUserWithTeam = namedtuple('mockUserWithTeam', field_names=['team'])
+    mock_user = mockUserWithTeam(team=models.Team(id=1, team_name='Testing'))
+
     def mock_read(db, user_id, user_model):
-        mockUserWithTeam = namedtuple('mockUserWithTeam', field_names=['team'])
-        mock_user = mockUserWithTeam(team=models.Team(**mock_team)) \
-            if mock_team is not None else None
+        if user_id == 0:
+            return None
         return mock_user
 
     monkeypatch.setattr(crud, 'read', mock_read)
     response = test_app.get(f'/users/{user_id}/team')
     assert response.status_code == status_code
-    assert response.json() == expected_response
+    assert response.json()[field] == value
 
 
 @pytest.mark.parametrize(
@@ -105,7 +111,7 @@ def test_update_user(
         email=email)
 
     def mock_read(db, uid, user_model):
-        if user_id == 0:  # user that does not exist
+        if user_id == 0:
             return None
         return mock_user
 
@@ -115,6 +121,7 @@ def test_update_user(
 
     monkeypatch.setattr(crud, 'read', mock_read)
     monkeypatch.setattr(crud, 'update_user', mock_update_user)
+
     payload = json.dumps({'user_schema': {'email': email}})
     response = test_app.put(f'/users/{user_id}', data=payload)
     assert response.status_code == status_code
