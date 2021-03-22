8458e960c2c73c7c5d24783efa737ffc53594bb6
Cole Rutledge <ColeRutledge@gmail.com>
2021-03-22 19:00:27 -0400

    :fire: remove keywords from crud funcs

--- app/routers/teams.py
+++ app/routers/teams.py
@@ -10,68 +10,62 @@ router = APIRouter()
 
 @router.get('/', response_model=list[schemas.Team])
 def get_all_teams(db: Session = Depends(get_db)):
-    return crud.read_all(db=db, model=models.Team)
+    return crud.read_all(db, models.Team)
 
 
 @router.get('/{team_id}', response_model=schemas.Team)
 def get_team(team_id: int, db: Session = Depends(get_db)):
-    db_team: models.Team = crud.read(db=db, id=team_id, model=models.Team)
+    db_team: models.Team = crud.read(db, team_id, models.Team)
     if db_team is None:
         raise HTTPException(
             status_code=404,
-            detail='Team not found',
-        )
+            detail='Team not found')
     return db_team
 
 
 @router.get('/{team_id}/projects', response_model=list[schemas.Project])
 def get_team_projects(team_id: int, db: Session = Depends(get_db)):
-    db_team: models.Team = crud.read(db=db, id=team_id, model=models.Team)
+    db_team: models.Team = crud.read(db, team_id, models.Team)
     if db_team is None:
         raise HTTPException(
             status_code=404,
-            detail='Project not found',
-        )
+            detail='Project not found')
     return db_team.projects
 
 
 @router.get('/{team_id}/users', response_model=list[schemas.User])
 def get_team_users(team_id: int, db: Session = Depends(get_db)):
-    db_team: models.Team = crud.read(db=db, id=team_id, model=models.Team)
+    db_team: models.Team = crud.read(db, team_id, models.Team)
     if db_team is None:
         raise HTTPException(
             status_code=404,
-            detail='Project not found',
-        )
+            detail='Project not found')
     return db_team.users
 
 
 @router.post('/', response_model=schemas.Team, status_code=status.HTTP_201_CREATED)
 def create_team(team: schemas.TeamBase, db: Session = Depends(get_db)):
-    return crud.create(db=db, body=team, model=models.Team)
+    return crud.create(db, team, models.Team)
 
 
 @router.put('/{team_id}', response_model=schemas.Team)
 def update_team(
-    team_id: int,
-    team: schemas.TeamBase = Body(..., embed=True),
-    db: Session = Depends(get_db),
-):
-    db_team: models.Team = crud.read(db=db, id=team_id, model=models.Team)
+        team_id: int,
+        team: schemas.TeamBase = Body(..., embed=True),
+        db: Session = Depends(get_db)):
+    db_team: models.Team = crud.read(db, team_id, models.Team)
     if db_team is None:
         raise HTTPException(
             status_code=404,
-            detail='Team not found',
-        )
-    return crud.update_team(db, schema=team, model=db_team)
+            detail='Team not found')
+    return crud.update_team(db, team, db_team)
 
 
 @router.delete('/{team_id}', response_model=schemas.Team)
 def delete_team(team_id: int, db: Session = Depends(get_db)):
-    db_team: models.Team = crud.read(db=db, id=team_id, model=models.Team)
+    db_team: models.Team = crud.read(db, team_id, models.Team)
     if db_team is None:
         raise HTTPException(
             status_code=404,
-            detail='Team not found',
-        )
-    return crud.delete(db=db, resource=db_team)
+            detail='Team not found')
+    return crud.delete(db, db_team)
