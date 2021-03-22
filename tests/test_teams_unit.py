e7364baad972b3bd30f612ada79b5a93122bfc10
Cole Rutledge <ColeRutledge@gmail.com>
2021-03-22 19:12:25 -0400

    :white_check_mark: unit test added: get_team

--- tests/test_teams_unit.py
+++ tests/test_teams_unit.py
@@ -2,7 +2,7 @@
 # from collections import namedtuple
 # from typing import Union
 
-# import pytest
+import pytest
 from fastapi.testclient import TestClient
 
 from app import models, crud
@@ -27,3 +27,29 @@ def test_get_all_teams(monkeypatch, test_app: TestClient):
     assert response.status_code == HTTP_200_OK
     assert response.json() == [
         {'id': 1, 'team_name': 'Engineering'}, {'id': 2, 'team_name': 'IT'}]
+
+
+@pytest.mark.parametrize(
+    argnames=['team_id', 'status_code', 'field', 'value'],
+    argvalues=[
+        (1, HTTP_200_OK, 'team_name', 'Engineering'),
+        (0, HTTP_404_NOT_FOUND, 'detail', 'Team not found')])
+def test_get_team(
+        team_id: int,
+        status_code: int,
+        field: str,
+        value: str,
+        monkeypatch,
+        test_app: TestClient):
+
+    mock_team = models.Team(id=1, team_name='Engineering')
+
+    def mock_read(*args):
+        if team_id == 0:  # team that does not exist
+            return None
+        return mock_team
+
+    monkeypatch.setattr(crud, 'read', mock_read)
+    response = test_app.get(f'/teams/{team_id}')
+    assert response.status_code == status_code
+    assert response.json()[field] == value
