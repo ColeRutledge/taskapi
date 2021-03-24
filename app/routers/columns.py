ad1f006408b657106418d547f785793240aebea9
Cole Rutledge <ColeRutledge@gmail.com>
2021-03-24 11:30:04 -0400

    :fire: remove keyword args for crud ops

--- app/routers/columns.py
+++ app/routers/columns.py
@@ -10,57 +10,52 @@ router = APIRouter()
 
 @router.get('/', response_model=list[schemas.Column])
 def get_all_columns(db: Session = Depends(get_db)):
-    return crud.read_all(db=db, model=models.Column)
+    return crud.read_all(db, models.Column)
 
 
 @router.get('/{column_id}', response_model=schemas.Column)
 def get_column(column_id: int, db: Session = Depends(get_db)):
-    db_column: models.Column = crud.read(db=db, id=column_id, model=models.Column)
+    db_column: models.Column = crud.read(db, column_id, models.Column)
     if db_column is None:
         raise HTTPException(
             status_code=404,
-            detail='Column not found',
-        )
+            detail='Column not found')
     return db_column
 
 
 @router.get('/{column_id}/tasks', response_model=list[schemas.Task])
 def get_column_tasks(column_id: int, db: Session = Depends(get_db)):
-    db_column: models.Column = crud.read(db=db, id=column_id, model=models.Column)
+    db_column: models.Column = crud.read(db, column_id, models.Column)
     if db_column is None:
         raise HTTPException(
             status_code=404,
-            detail='Column not found',
-        )
+            detail='Column not found')
     return db_column.tasks
 
 
 @router.post('/', response_model=schemas.Column, status_code=status.HTTP_201_CREATED)
 def create_column(column: schemas.ColumnBase, db: Session = Depends(get_db)):
-    return crud.create(db=db, body=column, model=models.Column)
+    return crud.create(db, column, models.Column)
 
 
 @router.put('/{column_id}', response_model=schemas.Column)
 def update_column(
-    column_id: int,
-    column: schemas.ColumnUpdate = Body(..., embed=True),
-    db: Session = Depends(get_db),
-):
-    db_column: models.Column = crud.read(db=db, id=column_id, model=models.Column)
+        column_id: int,
+        column: schemas.ColumnUpdate = Body(..., embed=True),
+        db: Session = Depends(get_db)):
+    db_column: models.Column = crud.read(db, column_id, models.Column)
     if db_column is None:
         raise HTTPException(
             status_code=404,
-            detail='Column not found',
-        )
-    return crud.update_column(db, schema=column, model=db_column)
+            detail='Column not found')
+    return crud.update_column(db, column, db_column)
 
 
 @router.delete('/{column_id}', response_model=schemas.Column)
 def delete_column(column_id: int, db: Session = Depends(get_db)):
-    db_column: models.Column = crud.read(db=db, id=column_id, model=models.Column)
+    db_column: models.Column = crud.read(db, column_id, models.Column)
     if db_column is None:
         raise HTTPException(
             status_code=404,
-            detail='Column not found',
-        )
-    return crud.delete(db=db, resource=db_column)
+            detail='Column not found')
+    return crud.delete(db, db_column)
