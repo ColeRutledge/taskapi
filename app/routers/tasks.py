717071fd4f8a639be8a57a055252246d984c5515
Cole Rutledge <ColeRutledge@gmail.com>
2021-03-24 12:20:44 -0400

    :fire: remove keywords from crud for testing

--- app/routers/tasks.py
+++ app/routers/tasks.py
@@ -10,46 +10,42 @@ router = APIRouter()
 
 @router.get('/', response_model=list[schemas.Task])
 def get_all_tasks(db: Session = Depends(get_db)):
-    return crud.read_all(db=db, model=models.Task)
+    return crud.read_all(db, models.Task)
 
 
 @router.get('/{task_id}', response_model=schemas.Task)
 def get_task(task_id: int, db: Session = Depends(get_db)):
-    db_task = crud.read(db=db, id=task_id, model=models.Task)
+    db_task = crud.read(db, task_id, models.Task)
     if db_task is None:
         raise HTTPException(
             status_code=404,
-            detail='Task not found',
-        )
+            detail='Task not found')
     return db_task
 
 
 @router.post('/', response_model=schemas.Task, status_code=status.HTTP_201_CREATED)
 def create_task(task: schemas.TaskBase, db: Session = Depends(get_db)):
-    return crud.create(db=db, body=task, model=models.Task)
+    return crud.create(db, task, models.Task)
 
 
 @router.put('/{task_id}', response_model=schemas.Task)
 def update_Task(
-    task_id: int,
-    task: schemas.TaskUpdate = Body(..., embed=True),
-    db: Session = Depends(get_db),
-):
-    db_task = crud.read(db=db, id=task_id, model=models.Task)
+        task_id: int,
+        task: schemas.TaskUpdate = Body(..., embed=True),
+        db: Session = Depends(get_db)):
+    db_task = crud.read(db, task_id, models.Task)
     if db_task is None:
         raise HTTPException(
             status_code=404,
-            detail='Task not found',
-        )
-    return crud.update_task(db, schema=task, model=db_task)
+            detail='Task not found')
+    return crud.update_task(db, task, db_task)
 
 
 @router.delete('/{task_id}', response_model=schemas.Task)
 def delete_task(task_id: int, db: Session = Depends(get_db)):
-    db_task = crud.read(db=db, id=task_id, model=models.Task)
+    db_task = crud.read(db, task_id, models.Task)
     if db_task is None:
         raise HTTPException(
             status_code=404,
-            detail='Task not found',
-        )
-    return crud.delete(db=db, resource=db_task)
+            detail='Task not found')
+    return crud.delete(db, db_task)
