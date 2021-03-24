4e6951da202b29801332c0c5f864715bd2532f01
Cole Rutledge <ColeRutledge@gmail.com>
2021-03-24 12:21:09 -0400

    :white_check_mark: adds unit test for get_all_tasks

---
+++ tests/test_tasks_unit.py
@@ -0,0 +1,174 @@
+# import json
+# from collections import namedtuple
+# from typing import Union
+
+# import pytest
+from fastapi.testclient import TestClient
+
+from app import models, crud
+
+
+HTTP_200_OK = 200
+HTTP_201_CREATED = 201
+HTTP_400_BAD_REQUEST = 400
+HTTP_404_NOT_FOUND = 404
+HTTP_401_UNAUTHORIZED = 401
+
+
+# models.Task(id=1, task_description='UnitTest', due_date=None, column_idx=0, column_id=1)
+# models.Task(id=2, task_description='SmokeTest', due_date=None, column_idx=1, column_id=1)
+
+def test_get_all_tasks(monkeypatch, test_app: TestClient):
+
+    def mock_read_all(*args):
+        mock_task1 = models.Task(
+            id=1, task_description='UnitTest', due_date=None, column_idx=0, column_id=1)
+        mock_task2 = models.Task(
+            id=2, task_description='SmokeTest', due_date=None, column_idx=1, column_id=1)
+        return [mock_task1, mock_task2]
+
+    monkeypatch.setattr(crud, 'read_all', mock_read_all)
+    response = test_app.get('/tasks/')
+    assert response.status_code == HTTP_200_OK
+    assert response.json() == [
+        {'id': 1, 'task_description': 'UnitTest',
+         'due_date': None, 'column_idx': 0, 'column_id': 1},
+        {'id': 2, 'task_description': 'SmokeTest',
+         'due_date': None, 'column_idx': 1, 'column_id': 1}]
+
+
+# @pytest.mark.parametrize(
+#     argnames=['task_id', 'status_code', 'field', 'value'],
+#     argvalues=[
+#         (1, HTTP_200_OK, 'task_description', 'Test'),
+#         (0, HTTP_404_NOT_FOUND, 'detail', 'task not found')])
+# def test_get_task(
+#         task_id: int,
+#         status_code: int,
+#         field: str,
+#         value: str,
+#         monkeypatch,
+#         test_app: TestClient):
+
+#     mock_task = models.task(id=1, task_description='Test', task_pos=0, project_id=1)
+
+#     def mock_read(*args):
+#         if task_id == 0:  # task that does not exist
+#             return None
+#         return mock_task
+
+#     monkeypatch.setattr(crud, 'read', mock_read)
+#     response = test_app.get(f'/tasks/{task_id}')
+#     assert response.status_code == status_code
+#     assert response.json()[field] == value
+
+
+# @pytest.mark.parametrize(
+#     argnames=['task_id', 'status_code', 'field', 'value'],
+#     argvalues=[
+#         (1, HTTP_200_OK, 1, {
+#             'id': 2, 'task_description': 'SmokeTest',
+#             'task_idx': 1, 'task_id': 1, 'due_date': None}),
+#         (0, HTTP_404_NOT_FOUND, 'detail', 'task not found')])
+# def test_get_task_tasks(
+#         task_id: int,
+#         status_code: int,
+#         field: str,
+#         value: Union[str, dict],
+#         monkeypatch,
+#         test_app: TestClient):
+
+#     MocktaskWithTasks = namedtuple('MocktaskWithTasks', ['tasks'])
+#     mock_task = MocktaskWithTasks(tasks=[
+#         models.Task(
+#             id=1, task_description='UnitTest',
+#             due_date=None, task_idx=0, task_id=1),
+#         models.Task(
+#             id=2, task_description='SmokeTest',
+#             due_date=None, task_idx=1, task_id=1)])
+
+#     def mock_read(*args):
+#         if task_id == 0:
+#             return None
+#         return mock_task
+
+#     monkeypatch.setattr(crud, 'read', mock_read)
+#     response = test_app.get(f'/tasks/{task_id}/tasks')
+#     assert response.status_code == status_code
+#     assert response.json()[field] == value
+
+
+# def test_create_task(monkeypatch, test_app: TestClient):
+
+#     def mock_create(*args):
+#         return models.task(id=1, task_description='UnitTest', task_pos=0, project_id=1)
+
+#     monkeypatch.setattr(crud, 'create', mock_create)
+#     payload = json.dumps({'task_description': 'UnitTest', 'task_pos': 0, 'project_id': 1})
+#     response = test_app.post('/tasks/', data=payload)
+#     assert response.status_code == HTTP_201_CREATED
+#     assert response.json() == {
+#         'id': 1, 'task_description': 'UnitTest', 'task_pos': 0, 'project_id': 1}
+
+
+# @pytest.mark.parametrize(
+#     argnames=['task_id', 'task_description', 'status_code', 'field', 'value'],
+#     argvalues=[
+#         (1, 'UnitTest', HTTP_200_OK, 'task_description', 'UnitTest'),
+#         (0, 'BadtaskID', HTTP_404_NOT_FOUND, 'detail', 'task not found')])
+# def test_update_task(
+#         task_id: int,
+#         task_description: str,
+#         status_code: int,
+#         field: str,
+#         value: Union[str, dict],
+#         monkeypatch,
+#         test_app: TestClient):
+
+#     def mock_read(*args):
+#         if task_id == 0:
+#             return None
+#         return models.task(id=1, task_description='Pre-Change', task_pos=0, project_id=1)
+
+#     def mock_update_task(*args):
+#         return models.task(id=1, task_description='UnitTest', task_pos=0, project_id=1)
+
+#     monkeypatch.setattr(crud, 'read', mock_read)
+#     monkeypatch.setattr(crud, 'update_task', mock_update_task)
+
+#     payload = json.dumps({'task': {
+#         'task_description': task_description, 'task_pos': 0, 'project_id': 1}})
+#     response = test_app.put(f'/tasks/{task_id}', data=payload)
+#     assert response.status_code == status_code
+#     assert response.json()[field] == value
+
+
+# @pytest.mark.parametrize(
+#     argnames=['task_id', 'status_code', 'field', 'value'],
+#     argvalues=[
+#         (1, HTTP_200_OK, 'task_description', 'UnitTest'),
+#         (0, HTTP_404_NOT_FOUND, 'detail', 'task not found')])
+# def test_delete_task(
+#         task_id: int,
+#         status_code: int,
+#         field: str,
+#         value: Union[str, dict],
+#         monkeypatch,
+#         test_app: TestClient):
+
+#     mock_task = models.task(id=1, task_description='UnitTest', task_pos=0, project_id=1)
+
+#     def mock_read(*args):
+#         if task_id == 0:
+#             return None
+#         return mock_task
+
+#     def mock_delete(*args):
+#         return mock_task
+
+#     monkeypatch.setattr(crud, 'read', mock_read)
+#     monkeypatch.setattr(crud, 'delete', mock_delete)
+
+#     response = test_app.delete(f'/tasks/{task_id}')
+#     assert response.status_code == status_code
+#     assert response.json()[field] == value
