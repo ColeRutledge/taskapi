25712d5b9b374861044156b8b07cb0945e29fb34
Cole Rutledge <ColeRutledge@gmail.com>
2021-03-26 13:00:22 -0400

    :white_check_mark: adds unit tests for update

--- tests/test_crud.py
+++ tests/test_crud.py
@@ -7,14 +7,22 @@ from app import crud, models, schemas
 @pytest.mark.parametrize(
     argnames=['Schema', 'Model', 'schema_vals', 'attributes', 'field', 'value'],
     argvalues=[
-        (schemas.UserCreate, models.User, {
-            'first_name': 'Test', 'last_name': 'User',
-            'email': 'test@user.com', 'password': 'badpassword'},
-         ['first_name', 'last_name', 'hashed_password', 'email'],
-         'email', 'test@user.com'),
-        (schemas.ProjectBase, models.Project, {
-            'project_name': 'Test', 'team_id': 1},
-         ['project_name', 'team_id'], 'project_name', 'Test')])
+        (
+            schemas.UserCreate,
+            models.User,
+            {
+                'first_name': 'Test', 'last_name': 'User',
+                'email': 'test@user.com', 'password': 'badpassword'},
+            ['first_name', 'last_name', 'hashed_password', 'email'],
+            'email',
+            'test@user.com'),
+        (
+            schemas.ProjectBase,
+            models.Project,
+            {'project_name': 'Test', 'team_id': 1},
+            ['project_name', 'team_id'],
+            'project_name',
+            'Test')])
 def test_create(
         Schema: crud.Schema,
         Model: crud.DatabaseModel,
@@ -25,12 +33,12 @@ def test_create(
         monkeypatch,
         test_db_seeded: Session):
 
-    schema = Schema(**schema_vals)
-
-    def mock_get_password_hash(password):
+    def mock_get_password_hash(*args):
         return 'fake_hashed_password'
 
     monkeypatch.setattr(models.User, 'get_password_hash', mock_get_password_hash)
+
+    schema = Schema(**schema_vals)
     orm_model = crud.create(test_db_seeded, schema, Model)
     assert getattr(orm_model, field) == value
     assert all(getattr(orm_model, field, False) for field in attributes)
@@ -39,10 +47,18 @@ def test_create(
 @pytest.mark.parametrize(
     argnames=['resource_id', 'Model', 'attributes', 'field', 'value'],
     argvalues=[
-        (1, models.User, [
-            'first_name', 'last_name', 'hashed_password', 'email'],
-         'email', 'bob@smith.com'),
-        (2, models.Project, ['project_name', 'team_id'], 'project_name', 'SEO')])
+        (
+            1,
+            models.User,
+            ['first_name', 'last_name', 'hashed_password', 'email'],
+            'email',
+            'bob@smith.com'),
+        (
+            2,
+            models.Project,
+            ['project_name', 'team_id'],
+            'project_name',
+            'SEO')])
 def test_read(
         resource_id: int,
         Model: crud.DatabaseModel,
@@ -59,10 +75,18 @@ def test_read(
 @pytest.mark.parametrize(
     argnames=['Model', 'attributes', 'field', 'value', 'length'],
     argvalues=[
-        (models.User, [
-            'first_name', 'last_name', 'hashed_password', 'email'],
-         'email', 'Sally@123.com', 4),
-        (models.Project, ['project_name', 'team_id'], 'project_name', 'SEO', 3)])
+        (
+            models.User,
+            ['first_name', 'last_name', 'hashed_password', 'email'],
+            'email',
+            'Sally@123.com',
+            4),
+        (
+            models.Project,
+            ['project_name', 'team_id'],
+            'project_name',
+            'SEO',
+            3)])
 def test_read_all(
         Model: crud.DatabaseModel,
         attributes: list[str],
@@ -75,3 +99,52 @@ def test_read_all(
     assert len(orm_model_list) == length
     assert getattr(orm_model_list[1], field) == value
     assert all(getattr(orm_model_list[1], field, False) for field in attributes)
+
+
+@pytest.mark.parametrize(
+    argnames=[
+        'resource_id',
+        'Schema',
+        'Model',
+        'schema_vals',
+        'attributes',
+        'field',
+        'updated_value'],
+    argvalues=[
+        (
+            1,
+            schemas.UserUpdate,
+            models.User,
+            {'password': 'badpassword'},
+            ['first_name', 'last_name', 'hashed_password', 'email'],
+            'hashed_password',
+            'fake_hashed_password'),
+        (
+            2,
+            schemas.ProjectUpdate,
+            models.Project,
+            {'project_name': 'Deploy'},
+            ['project_name', 'team_id'],
+            'project_name',
+            'Deploy')])
+def test_update(
+        resource_id: int,
+        Schema: crud.Schema,
+        Model: crud.DatabaseModel,
+        schema_vals: dict,
+        attributes: list[str],
+        field: str,
+        updated_value: str,
+        monkeypatch,
+        test_db_seeded: Session):
+
+    def mock_get_password_hash(*args):
+        return 'fake_hashed_password'
+
+    monkeypatch.setattr(models.User, 'get_password_hash', mock_get_password_hash)
+
+    schema = Schema(**schema_vals)
+    orm_model = crud.read(test_db_seeded, resource_id, Model)
+    updated_orm_model = crud.update(test_db_seeded, schema, orm_model)
+    assert getattr(updated_orm_model, field) == updated_value
+    assert all(getattr(updated_orm_model, field, False) for field in attributes)
