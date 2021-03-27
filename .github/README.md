f4280a6b5578368fdd2c11974242c7cbe5aa64fc
Cole Rutledge <ColeRutledge@gmail.com>
2021-03-27 18:02:57 -0400

    :rocket: adds new workflow for testing and badge

--- .github/README.md
+++ .github/README.md
@@ -3,3 +3,4 @@
 <a href='https://asana-fastapi.herokuapp.com/docs'>Live Link</a>
 
 ![Continuous Integration and Delivery](https://github.com/ColeRutledge/asana_fastapi/actions/workflows/ci.yml/badge.svg?branch=master)
+![Test Coverage](https://img.shields.io/badge/Test%20Coverage-75%25-yellow)
---
+++ .github/update_badges.py
@@ -0,0 +1,27 @@
+#! /usr/bin/env python3
+from pathlib import Path
+import subprocess
+import re
+
+
+def determine_color(percentage: int):
+    if percentage >= 95:
+        return 'brightgreen'
+    elif percentage >= 75:
+        return 'yellow'
+    else:
+        return 'red'
+
+
+if __name__ == '__main__':
+    coverage = subprocess.run(['coverage', 'report'], capture_output=True, text=True)
+    PERCENTAGE = coverage.stdout.split()[-1][:-1]
+    COLOR = determine_color(int(PERCENTAGE))
+
+    path_to_readme = Path() / '.github' / 'README.md'
+    readme = open(path_to_readme).read()
+    readme = re.sub(r'-\d+%25', f'-{PERCENTAGE}%25', readme)
+    readme = re.sub(r'%25-\w+', f'%25-{COLOR}', readme)
+
+    with open(path_to_readme, mode='w') as file:
+        file.write(readme)
