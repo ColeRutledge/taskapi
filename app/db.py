b59c4231e69c573fd4991d67134e64f181dcc4dd
Cole Rutledge <ColeRutledge@gmail.com>
2021-03-31 20:01:15 -0400

    :package: move db engine creation to func for testing

--- app/db.py
+++ app/db.py
@@ -6,16 +6,20 @@ from sqlalchemy.orm import sessionmaker
 from app.config import get_settings
 
 
-SQLALCHEMY_DATABASE_URL = get_settings().db_url
+def create_database_engine():
+    SQLALCHEMY_DATABASE_URL = get_settings().db_url
 
-if os.getenv('FASTAPI_ENV'):        # check if running in container
-    engine = create_engine(SQLALCHEMY_DATABASE_URL)
-else:
-    engine = create_engine(
-        'sqlite:///app.db',
-        connect_args={'check_same_thread': False})
+    if os.getenv('FASTAPI_ENV'):        # check if running in container
+        engine = create_engine(SQLALCHEMY_DATABASE_URL)
+    else:
+        engine = create_engine(
+            'sqlite:///app.db',
+            connect_args={'check_same_thread': False})
+    return engine
 
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+SessionLocal = sessionmaker(
+    autocommit=False, autoflush=False, bind=create_database_engine())
 
 
 def get_db():
